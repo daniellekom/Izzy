cd9cb3a300736afcf1d3b23fb4206bc7830417b1
Danielle Kom <97759611+daniellekom@users.noreply.github.com>
2022-12-11 12:41:52 -0500

    more

--- test/basics/if_while_for.dart
+++ test/basics/if_while_for.dart
@@ -1,1 +1,24 @@
-void mai
+void main() {
+  int fizzBuzz(String value) {
+    if (value == "fiz") {
+      return 3;
+    } else if (value == "buz") {
+      return 5;
+    } else {
+      return -1;
+    }
+  }
+
+  print("${fizzBuzz("lksdj")}");
+}
+
+//function that receive fizz or buzz String or else...
+//in case fiz return 3
+//in case buz return 5
+//else return -1
+
+//functions rules:
+// 1. returns or not -> void or not void(String,int,list etc)
+// 2. function name
+// 3. receives arguments or not
+// 4. what is the function doing?
